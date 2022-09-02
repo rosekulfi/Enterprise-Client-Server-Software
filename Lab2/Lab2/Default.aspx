d6bde97686300484caf52558c18565bea6d0f6dd
rosekulfi <112670686+rosekulfi@users.noreply.github.com>
2022-09-01 21:48:02 -0400

    Add files via upload
    Temperature convertor webpage from Fahrenheit to Celsius

---
+++ Lab2/Lab2/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lab 2 - Anika Sharma</title>
+</head>
+<body style="height: 297px; width: 394px">
+    <form id="form1" runat="server">
+        <div style="height: 222px; width: 382px;">
+        <asp:Panel ID="Panel1" runat="server" BackColor="#CC99FF" Height="218px" BorderColor="Black" BorderStyle="Solid" Direction="LeftToRight" Font-Bold="True" Font-Size="X-Large" HorizontalAlign="Center">
+            <br />
+            <br />
+            Temperature
+            <br />
+            Converter<br />
+            <div>
+                <asp:Label ID="Label1" runat="server" Font-Bold="False" Font-Size="Medium" Text="Fahrenheit: "></asp:Label>
+                &nbsp;
+                <asp:TextBox ID="fahTextBox" runat="server" Width="158px"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Label ID="Label2" runat="server" Font-Bold="False" Font-Size="Medium" Text="Centregrade:"></asp:Label>
+                &nbsp;&nbsp;
+                <asp:TextBox ID="celTextBox" runat="server" Width="150px"></asp:TextBox>
+                <div>
+                    <asp:Label ID="errorMsg" runat="server" BackColor="White" Font-Bold="False" Font-Italic="False" Font-Size="Medium" ForeColor="Red"></asp:Label>
+                </div>
+            </div>
+        </asp:Panel>
+        </div>
+        <asp:Panel ID="Panel2" runat="server" BorderColor="Black" BorderStyle="Solid" Height="62px" Width="376px">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="clearButton" runat="server" Text="Clear" OnClick="clearButton_Click" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="convertButton" runat="server" Text="Convert" OnClick="convertButton_Click" />
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Lab2/Lab2/Default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //clear button will clear both the textboxes and any error message
+        protected void clearButton_Click(object sender, EventArgs e)
+        {
+            fahTextBox.Text = "";
+            celTextBox.Text = "";
+            errorMsg.Text = "";
+        }
+
+        //convert button will convert the temperature
+        protected void convertButton_Click(object sender, EventArgs e) 
+        {
+            //if neither textbox has text, throw error "Enter one value."
+            if (fahTextBox.Text == "" && celTextBox.Text == "")
+            {
+                errorMsg.Text = "Enter one value.";
+
+            }
+
+            //if both textboxes have text, throw error "Enter one value."
+            else if (fahTextBox.Text != "" && celTextBox.Text != "") 
+            {
+                errorMsg.Text = "Enter one value.";   
+            }
+
+            //if only one textbox has text and it is not a float, throw error "Please enter a correct value."
+            else if (fahTextBox.Text != "" && celTextBox.Text == "") 
+            {
+                float i = 0;
+                string s = fahTextBox.Text;
+                bool result = float.TryParse(s, out i);
+
+                if(result == false) 
+                {
+                    errorMsg.Text = "Please enter a correct value.";
+                }
+                else if(result == true) 
+                {
+                    errorMsg.Text = "";
+                    //float k = 5 / 9;
+                    float j = (i - 32F) * (5F/9F);
+                    celTextBox.Text = j.ToString();
+                }
+            }
+            else if (fahTextBox.Text == "" && celTextBox.Text != "") 
+            {
+                float i = 0;
+                string s = celTextBox.Text;
+                bool result = float.TryParse(s, out i);
+
+                if(result == false) 
+                {
+                    errorMsg.Text = "Please enter a correct value";
+                }
+                else if(result == true) 
+                {
+                    errorMsg.Text = "";
+                    float j = i * (9F/5F) + 32F;
+                    fahTextBox.Text = j.ToString();
+                }
+            }
+
+        }
+
+    }
+}
