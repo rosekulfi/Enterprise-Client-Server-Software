5b8626ffff4b680f78739eb7247a72d781b35a56
rosekulfi <112670686+rosekulfi@users.noreply.github.com>
2022-09-01 22:00:41 -0400

    Add files via upload
    Anagram calculator

---
+++ Lab3/Lab3/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab3.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lab 3</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 398px">
+            <asp:Panel ID="Panel1" runat="server" BackColor="Silver" BorderStyle="Solid" Font-Size="Large" Height="349px" Width="282px">
+&nbsp;<br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="Label1" runat="server" Text="Find Anagrams" Font-Bold="True" Font-Size="XX-Large"></asp:Label>
+                <br />
+                <br />
+                &nbsp;
+                <br />
+                &nbsp;
+                <asp:Label ID="Label2" runat="server" Font-Bold="False" Font-Size="Medium" Text="Enter a character string: "></asp:Label>
+                &nbsp;<asp:TextBox ID="stringInput" runat="server" Height="16px" Width="70px"></asp:TextBox>
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;
+                <asp:CheckBox ID="elimDups" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" />
+                <asp:Label ID="Label3" runat="server" Font-Size="Medium" Text="Eliminate Duplicates"></asp:Label>
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:ListBox ID="anagramsList" runat="server" Width="145px" OnSelectedIndexChanged="anagramsList_SelectedIndexChanged"></asp:ListBox>
+                <br />
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="findAnagrams" runat="server" Height="21px" OnClick="findAnagrams_Click" Text="Find Anagrams" Width="105px" />
+                <br />
+                <br />
+                &nbsp;&nbsp;
+                <asp:Label ID="bottomText" runat="server" Font-Size="Small"></asp:Label>
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab3/Lab3/Default.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        int anagramTotal = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Swap characters in order to create new anagrams
+        private string Swap(int length, string str, int frontVal, int backVal) 
+        {
+            char frontValCopy = str[frontVal];
+            char backValCopy = str[backVal];
+            string finalStr = "";
+
+            for (int i = 0; i < length; i++) 
+            {
+                if(i == frontVal)
+                {
+                    finalStr = finalStr + backValCopy;
+                }
+                else if(i == backVal)
+                {
+                    finalStr = finalStr + frontValCopy;
+                }
+                else
+                {
+                    finalStr = finalStr + str[i];
+                } 
+            }
+
+            return finalStr;
+        }
+
+        private void AnagramCalculator(string str, bool noDups, int frontVal, int backVal) 
+        {
+            int length = str.Length;
+            if(frontVal == backVal)
+            {
+                //check for duplicates by searching in listbox
+                if (noDups)
+                {
+                    ListItem temp = anagramsList.Items.FindByText(str);
+                    if (temp != null) return;
+                }
+
+                anagramsList.Items.Add(str);
+                anagramTotal++;
+                return;
+            }
+
+            for(int i = frontVal; i <= backVal; i++)
+            {
+                str = Swap(length, str, frontVal, i);
+                AnagramCalculator(str, noDups, frontVal + 1, backVal);
+                str = Swap(length, str, frontVal, i);
+            }
+        }
+
+        protected void findAnagrams_Click(object sender, EventArgs e)
+        {
+            //clear list box and bottom text message
+            anagramsList.Items.Clear();
+            anagramTotal = 0;
+            bottomText.Text = "";
+
+            //check string is between 1-6 chars
+            string input = stringInput.Text;
+            stringInput.Text = "";
+            int inputLen = input.Length;
+
+            if (input.Length > 6 || input.Length < 1)
+            {
+                bottomText.Text = "Please enter a string from 1 to 6 characters";
+                return;
+            }
+            
+            //check if checkbox is clicked
+            bool noDups = elimDups.Checked;
+
+            //find anagrams
+            AnagramCalculator(input, noDups, 0, inputLen-1);
+
+            //print out total number of anagrams and clear textbox
+            bottomText.Text = anagramTotal + " anagrams found.";
+        }
+
+        protected void anagramsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
